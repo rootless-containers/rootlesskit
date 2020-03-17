221d3be244551824ee363f2f951caf19c49a77ba
Kir Kolyshkin <kolyshkin@gmail.com>
2020-03-16 20:18:28 -0700

    Use moby/sys/mountinfo
    Drop the implementation forked from runc, use moby/sys/mountinfo.
    
    Use SingleEntryFilter as we're only interested in / mount.
    
    NOTE we're no longer checking for m.Root == "/", only for
    the mount point.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- pkg/parent/mount/mount.go
+++
@@ -1,7 +0,0 @@
-// Package mount was forked from https://github.com/opencontainers/runc/tree/fc5759cf4fcf3f9c77c5973a24d37188dbcc92ee/libcontainer/mount
-package mount
-
-// GetMounts retrieves a list of mounts for the current running process.
-func GetMounts() ([]*Info, error) {
-	return parseMountTable()
-}
