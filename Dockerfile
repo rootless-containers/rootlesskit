rootless-containers/rootlesskit

commit 3e782ae6d8bc6c4954ce219ce8a6513db4415767

Write `$ROOTLESSKIT_STATE_DIR/resolv.conf`
The file might be useful for detach-netns mode

Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

pkg/child/child.go
@@ -159,6 +159,19 @@ func setupNet(stateDir string, msg *messages.ParentInitNetworkDriverCompleted, e
 		return nil
 	}
 
+	stateDirResolvConf := filepath.Join(stateDir, "resolv.conf")
+	if err := os.WriteFile(stateDirResolvConf, generateResolvConf(msg.DNS), 0644); err != nil {
+		return fmt.Errorf("writing %s: %w", stateDirResolvConf, err)
+	}
+	hostsContent, err := generateEtcHosts()
+	if err != nil {
+		return err
+	}
+	stateDirHosts := filepath.Join(stateDir, "hosts")
+	if err := os.WriteFile(stateDirHosts, hostsContent, 0644); err != nil {
+		return fmt.Errorf("writing %s: %w", stateDirHosts, err)
+	}
+
 	if detachedNetNSPath == "" {
 		// non-detached mode
 		if err := activateLoopback(); err != nil {
@@ -172,23 +185,26 @@ func setupNet(stateDir string, msg *messages.ParentInitNetworkDriverCompleted, e
 			return err
 		}
 		if etcWasCopied {
-			if err := writeResolvConf(msg.DNS); err != nil {
-				return err
-			}
-			if err := writeEtcHosts(); err != nil {
-				return err
+			// remove copied-up link
+			for _, f := range []string{"/etc/resolv.conf", "/etc/hosts"} {
+				if err := os.RemoveAll(f); err != nil {
+					return fmt.Errorf("failed to remove copied-up link %q: %w", f, err)
+				}
+				if err := os.WriteFile(f, []byte{}, 0644); err != nil {
+					return fmt.Errorf("writing %s: %w", f, err)
+				}
 			}
 		} else {
 			logrus.Warn("Mounting /etc/resolv.conf without copying-up /etc. " +
 				"Note that /etc/resolv.conf in the namespace will be unmounted when it is recreated on the host. " +
 				"Unless /etc/resolv.conf is statically configured, copying-up /etc is highly recommended. " +
 				"Please refer to RootlessKit documentation for further information.")
-			if err := mountResolvConf(stateDir, msg.DNS); err != nil {
-				return err
-			}
-			if err := mountEtcHosts(stateDir); err != nil {
-				return err
-			}
+		}
+		if err := unix.Mount(stateDirResolvConf, "/etc/resolv.conf", "", uintptr(unix.MS_BIND), ""); err != nil {
+			return fmt.Errorf("failed to create bind mount /etc/resolv.conf for %s: %w", stateDirResolvConf, err)
+		}
+		if err := unix.Mount(stateDirHosts, "/etc/hosts", "", uintptr(unix.MS_BIND), ""); err != nil {
+			return fmt.Errorf("failed to create bind mount /etc/hosts for %s: %w", stateDirHosts, err)
 		}
 	} else {
 		// detached mode
@@ -206,7 +222,6 @@ func setupNet(stateDir string, msg *messages.ParentInitNetworkDriverCompleted, e
 		}); err != nil {
 			return err
 		}
-		// TODO: write /etc/resolv.conf and /etc/hosts in a custom directory?
 	}
 	return nil
 }

pkg/child/hosts.go
@@ -3,9 +3,6 @@ package child
 import (
 	"fmt"
 	"os"
-	"path/filepath"
-
-	"golang.org/x/sys/unix"
 )
 
 // generateEtcHosts makes sure the current hostname is resolved into
@@ -25,37 +22,4 @@ func generateEtcHosts() ([]byte, error) {
 	s := fmt.Sprintf("%s\n127.0.0.1 %s\n::1 %s\n",
 		string(etcHosts), hostname, hostname)
 	return []byte(s), nil
-}
-
-// writeEtcHosts is akin to writeResolvConf
-// TODO: dedupe
-func writeEtcHosts() error {
-	newEtcHosts, err := generateEtcHosts()
-	if err != nil {
-		return err
-	}
-	// remove copied-up link
-	_ = os.Remove("/etc/hosts")
-	if err := os.WriteFile("/etc/hosts", newEtcHosts, 0644); err != nil {
-		return fmt.Errorf("writing /etc/hosts: %w", err)
-	}
-	return nil
-}
-
-// mountEtcHosts is akin to mountResolvConf
-// TODO: dedupe
-func mountEtcHosts(tempDir string) error {
-	newEtcHosts, err := generateEtcHosts()
-	if err != nil {
-		return err
-	}
-	myEtcHosts := filepath.Join(tempDir, "hosts")
-	if err := os.WriteFile(myEtcHosts, newEtcHosts, 0644); err != nil {
-		return fmt.Errorf("writing %s: %w", myEtcHosts, err)
-	}
-
-	if err := unix.Mount(myEtcHosts, "/etc/hosts", "", uintptr(unix.MS_BIND), ""); err != nil {
-		return fmt.Errorf("failed to create bind mount /etc/hosts for %s: %w", myEtcHosts, err)
-	}
-	return nil
 }

pkg/child/resolvconf.go
@@ -1,42 +1,5 @@
 package child
 
-import (
-	"fmt"
-	"os"
-	"path/filepath"
-
-	"golang.org/x/sys/unix"
-)
-
 func generateResolvConf(dns string) []byte {
 	return []byte("nameserver " + dns + "\n")
-}
-
-func writeResolvConf(dns string) error {
-	// remove copied-up link
-	_ = os.Remove("/etc/resolv.conf")
-	if err := os.WriteFile("/etc/resolv.conf", generateResolvConf(dns), 0644); err != nil {
-		return fmt.Errorf("writing %s: %w", "/etc/resolv.conf", err)
-	}
-	return nil
-}
-
-// mountResolvConf does not work when /etc/resolv.conf is a managed by
-// systemd or NetworkManager, because our bind-mounted /etc/resolv.conf (in our namespaces)
-// is unexpectedly unmounted when /etc/resolv.conf is recreated in the initial initial namespace.
-//
-// If /etc/resolv.conf is a symlink, e.g. to ../run/systemd/resolve/stub-resolv.conf,
-// our bind-mounted /etc/resolv.conf is still unmounted when /run/systemd/resolve/stub-resolv.conf is recreated.
-//
-// Use writeResolvConf with copying-up /etc for most cases.
-func mountResolvConf(tempDir, dns string) error {
-	myResolvConf := filepath.Join(tempDir, "resolv.conf")
-	if err := os.WriteFile(myResolvConf, generateResolvConf(dns), 0644); err != nil {
-		return fmt.Errorf("writing %s: %w", myResolvConf, err)
-	}
-
-	if err := unix.Mount(myResolvConf, "/etc/resolv.conf", "", uintptr(unix.MS_BIND), ""); err != nil {
-		return fmt.Errorf("failed to create bind mount /etc/resolv.conf for %s: %w", myResolvConf, err)
-	}
-	return nil
 }

commit 31f0953664f2626eef5367f571cf8ebdbc1110ed

lxc-user-nic: fix `/etc/resolv.conf` missing IP
Fix moby/moby issue 47327

This was a regression in 3e782ae6d8bc6c4954ce219ce8a6513db4415767
(v2.0.0-beta.0)

Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

.github/workflows/main.yaml
@@ -39,6 +39,22 @@ jobs:
       run: docker run --rm --net=host --privileged rootlesskit:test-integration ./integration-port.sh
     - name: "Integration test: IPv6 routing"
       run: docker run  --rm --privileged --sysctl net.ipv6.conf.all.disable_ipv6=0 rootlesskit:test-integration ./integration-ipv6.sh
+    - name: "Integration test: Network (network driver=slirp4netns)"
+      run: |
+        docker run --rm --privileged rootlesskit:test-integration ./integration-net.sh slirp4netns
+        docker run --rm --privileged rootlesskit:test-integration ./integration-net.sh slirp4netns --detach-netns
+    - name: "Integration test: Network (network driver=vpnkit)"
+      run: |
+        docker run --rm --privileged rootlesskit:test-integration ./integration-net.sh vpnkit
+        docker run --rm --privileged rootlesskit:test-integration ./integration-net.sh vpnkit --detach-netns
+    - name: "Integration test: Network (network driver=lxc-user-nic)"
+      run: |
+        docker run --rm --privileged rootlesskit:test-integration ./integration-net.sh lxc-user-nic
+        docker run --rm --privileged rootlesskit:test-integration ./integration-net.sh lxc-user-nic --detach-netns
+    - name: "Integration test: Network (network driver=pasta)"
+      run: |
+        docker run --rm --privileged rootlesskit:test-integration ./integration-net.sh pasta
+        docker run --rm --privileged rootlesskit:test-integration ./integration-net.sh pasta --detach-netns
 # ===== Benchmark: Network (MTU=1500) =====
     - name: "Benchmark: Network (MTU=1500, network driver=slirp4netns)"
       run: |

Dockerfile
@@ -62,7 +62,8 @@ FROM ubuntu:${UBUNTU_VERSION} AS test-integration
 # busybox: only for debugging purpose
 # sudo: only for lxc-user-nic benchmark and rootful veth benchmark (for comparison)
 # libcap2-bin and curl: used by the RUN instructions in this Dockerfile.
-RUN apt-get update && apt-get install -y iproute2 liblxc-common lxc-utils iperf3 busybox sudo libcap2-bin curl
+# bind9-dnsutils: for `nslookup` command used by integration-net.sh
+RUN apt-get update && apt-get install -y iproute2 liblxc-common lxc-utils iperf3 busybox sudo libcap2-bin curl bind9-dnsutils
 COPY --from=idmap /usr/bin/newuidmap /usr/bin/newuidmap
 COPY --from=idmap /usr/bin/newgidmap /usr/bin/newgidmap
 RUN /sbin/setcap cap_setuid+eip /usr/bin/newuidmap && \

hack/integration-net.sh
@@ -1,0 +1,21 @@
+#!/bin/bash
+# Integration tests for network drivers.
+# See also: benchmark-iperf3-net.sh
+
+source $(realpath $(dirname $0))/common.inc.sh
+if [ $# -lt 1 ]; then
+	ERROR "Usage: $0 NETDRIVER [FLAGS...]"
+	exit 1
+fi
+net=$1
+shift 1
+flags=$@
+INFO "net=${net} flags=$@"
+
+# Test DNS
+set -x
+if [ "${net}" = "lxc-user-nic" ]; then
+	# ignore "lxc-net is already running" error
+	sudo /usr/lib/$(uname -m)-linux-gnu/lxc/lxc-net start || true
+fi
+$ROOTLESSKIT --net=${net} --copy-up=/etc --copy-up=/run --disable-host-loopback ${flags} -- nslookup example.com

pkg/child/child.go
@@ -153,6 +153,9 @@ func setupCopyDir(driver copyup.ChildDriver, dirs []string) (bool, error) {
 	return false, nil
 }
 
+// setupNet sets up the network driver.
+//
+// NOTE: msg is altered during calling driver.ConfigureNetworkChild
 func setupNet(stateDir string, msg *messages.ParentInitNetworkDriverCompleted, etcWasCopied bool, driver network.ChildDriver, detachedNetNSPath string) error {
 	// HostNetwork
 	if driver == nil {
@@ -160,9 +163,6 @@ func setupNet(stateDir string, msg *messages.ParentInitNetworkDriverCompleted, e
 	}
 
 	stateDirResolvConf := filepath.Join(stateDir, "resolv.conf")
-	if err := os.WriteFile(stateDirResolvConf, generateResolvConf(msg.DNS), 0644); err != nil {
-		return fmt.Errorf("writing %s: %w", stateDirResolvConf, err)
-	}
 	hostsContent, err := generateEtcHosts()
 	if err != nil {
 		return err
@@ -177,9 +177,12 @@ func setupNet(stateDir string, msg *messages.ParentInitNetworkDriverCompleted, e
 		if err := activateLoopback(); err != nil {
 			return err
 		}
-		dev, err := driver.ConfigureNetworkChild(msg, detachedNetNSPath)
+		dev, err := driver.ConfigureNetworkChild(msg, detachedNetNSPath) // alters msg
 		if err != nil {
 			return err
+		}
+		if err := os.WriteFile(stateDirResolvConf, generateResolvConf(msg.DNS), 0644); err != nil {
+			return fmt.Errorf("writing %s: %w", stateDirResolvConf, err)
 		}
 		if err := activateDev(dev, msg.IP, msg.Netmask, msg.Gateway, msg.MTU); err != nil {
 			return err
@@ -213,9 +216,12 @@ func setupNet(stateDir string, msg *messages.ParentInitNetworkDriverCompleted, e
 		}); err != nil {
 			return err
 		}
-		dev, err := driver.ConfigureNetworkChild(msg, detachedNetNSPath)
+		dev, err := driver.ConfigureNetworkChild(msg, detachedNetNSPath) // alters msg
 		if err != nil {
 			return err
+		}
+		if err := os.WriteFile(stateDirResolvConf, generateResolvConf(msg.DNS), 0644); err != nil {
+			return fmt.Errorf("writing %s: %w", stateDirResolvConf, err)
 		}
 		if err := ns.WithNetNSPath(detachedNetNSPath, func(_ ns.NetNS) error {
 			return activateDev(dev, msg.IP, msg.Netmask, msg.Gateway, msg.MTU)