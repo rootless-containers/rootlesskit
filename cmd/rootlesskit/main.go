998ce8f2a4b35240ad333323c7e32af3f49b9b75
Anthony Rusdi <anthony.rusdi@gmail.com>
2024-04-01 15:46:13 +0700

    feat: add new none network
    Signed-off-by: Anthony Rusdi <anthony.rusdi@gmail.com>

--- cmd/rootlesskit/main.go
+++ cmd/rootlesskit/main.go
@@ -21,6 +21,7 @@ import (
 	"github.com/rootless-containers/rootlesskit/v2/pkg/network/pasta"
 	"github.com/rootless-containers/rootlesskit/v2/pkg/network/slirp4netns"
 	"github.com/rootless-containers/rootlesskit/v2/pkg/network/vpnkit"
+	"github.com/rootless-containers/rootlesskit/v2/pkg/network/none"
 	"github.com/rootless-containers/rootlesskit/v2/pkg/parent"
 	"github.com/rootless-containers/rootlesskit/v2/pkg/port/builtin"
 	"github.com/rootless-containers/rootlesskit/v2/pkg/port/portutil"
@@ -83,7 +84,7 @@ See https://rootlesscontaine.rs/getting-started/common/ .
 		}, CategoryState),
 		Categorize(&cli.StringFlag{
 			Name:  "net",
-			Usage: "network driver [host, pasta(experimental), slirp4netns, vpnkit, lxc-user-nic(experimental)]",
+			Usage: "network driver [host, none, pasta(experimental), slirp4netns, vpnkit, lxc-user-nic(experimental)]",
 			Value: "host",
 		}, CategoryNetwork),
 		Categorize(&cli.StringFlag{
@@ -368,7 +369,7 @@ func createParentOpt(clicontext *cli.Context, pipeFDEnvKey, stateDirEnvKey, pare
 	}
 
 	disableHostLoopback := clicontext.Bool("disable-host-loopback")
-	if !disableHostLoopback && clicontext.String("net") != "host" {
+	if !disableHostLoopback && clicontext.String("net") != "host" && clicontext.String("net") != "none" {
 		logrus.Warn("specifying --disable-host-loopback is highly recommended to prohibit connecting to 127.0.0.1:* on the host namespace (requires pasta, slirp4netns, or VPNKit)")
 	}
 
@@ -387,6 +388,26 @@ func createParentOpt(clicontext *cli.Context, pipeFDEnvKey, stateDirEnvKey, pare
 		}
 		if ifname != "" {
 			return opt, errors.New("ifname cannot be specified for --net=host")
+		}
+	case "none":
+		if mtu != 0 {
+			logrus.Warnf("unsupported mtu for --net=none: %d", mtu)
+		}
+		if ipnet != nil {
+			return opt, errors.New("custom cidr is not supported for --net=none")
+		}
+		if ifname != "" {
+			return opt, errors.New("ifname cannot be specified for --net=none")
+		}
+		switch portDriver := clicontext.String("port-driver"); portDriver {
+		case "none", "builtin":
+			// NOP
+		default:
+			return opt, errors.New("network \"none\" requires either port driver \"none\" or \"builtin\"")
+		}
+		opt.NetworkDriver, err = none.NewParentDriver()
+		if err != nil {
+			return opt, err
 		}
 	case "pasta":
 		logrus.Warn("\"pasta\" network driver is experimental. Needs very recent version of pasta (see docs/network.md).")
@@ -581,6 +602,8 @@ func createChildOpt(clicontext *cli.Context, pipeFDEnvKey, stateDirEnvKey string
 	}
 	switch s := clicontext.String("net"); s {
 	case "host":
+		// NOP
+	case "none":
 		// NOP
 	case "pasta":
 		opt.NetworkDriver = pasta.NewChildDriver()
