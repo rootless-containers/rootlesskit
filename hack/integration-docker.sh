967eeea0d88b20110f1af878445dced04a1dadbb
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2026-03-15 03:57:58 +0900

    hack/integration-docker.sh: increase sleep
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- hack/integration-docker.sh
+++ hack/integration-docker.sh
@@ -3,19 +3,23 @@ source $(realpath $(dirname $0))/common.inc.sh
 
 nonloopback="$(hostname -I | awk '{print $1}')"
 
+
+SLEEP_SECS=2
+[ "$DOCKERD_ROOTLESS_ROOTLESSKIT_PORT_DRIVER" = "implicit" ] && SLEEP_SECS=10
+
 docker rm -f nginx >/dev/null 2>&1 || true
 
 CURL="curl -fsSL"
 set -x
 
 docker run -d --name=nginx -p 8080:80 nginx:alpine
-sleep 2
+sleep "$SLEEP_SECS"
 $CURL "http://127.0.0.1:8080"
 $CURL "http://${nonloopback}:8080"
 docker rm -f nginx
 
 docker run -d --name=nginx -p 127.0.0.1:8080:80 nginx:alpine
-sleep 2
+sleep "$SLEEP_SECS"
 $CURL "http://127.0.0.1:8080"
 $CURL "http://${nonloopback}:8080" && ( ERROR "should fail"; exit 1 )
 docker rm -f nginx
@@ -24,7 +28,7 @@ docker rm -f nginx
 # "Error starting userland proxy: listen tcp4 172.18.0.2:8080: bind: cannot assign requested address."
 if [ "$DOCKERD_ROOTLESS_ROOTLESSKIT_PORT_DRIVER" != "implicit" ]; then
 	docker run -d --name=nginx -p "${nonloopback}:8080:80" nginx:alpine
-	sleep 2
+	sleep "$SLEEP_SECS"
 	$CURL "http://127.0.0.1:8080" && ( ERROR "should fail"; exit 1 )
 	$CURL "http://${nonloopback}:8080"
 	docker rm -f nginx
